58ef5b617527f6cb86a476bd9e08efecea59f32b
vishnu <vjp5266@psu.edu>
2022-07-16 16:26:25 -0700

    init

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+let fristname = "jonas"; //declaring a variable
+
+/* conventiona and rules for naming variables 
+camelcasing, underscores, hyphens to combine words
+never start with a number or use any symbols except _ and $
+no keywords
+name is a reserved keywords but is allwoed to use. dont use it tho
+variables dont start with a capital letter, convention
+when declaring constants, use all caps
+mnemonic variables
+*/
